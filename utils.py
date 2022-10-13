e0a5af76e1573f5c38d7dcd1d5d14acfffbe9ee3
jerry <jerry.jge@gmail.com>
2022-10-13 13:04:18 +0800

    imported a compute bleu score function

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 import torch
+from typing import List
+import sacrebleu
+from nmt_model import Hypothesis
 
 def save_checkpoint(state, filename="checkpoint.pth.tar"):
     print("<ea> saving checkpoint")
@@ -9,3 +12,30 @@ def load_checkpoint(checkpoint, model, optimizer):
     model.load_state_dict(checkpoint['state_dict'])
     optimizer.load_state_dict(checkpoint['optimizer'])
     #if adding other parameters, use "checkpoint['best_curr_accuracy']" for parameter "best_curr_accuracy"
+
+def translate_sentence(model, input_sentence, source_language, target_language, device, max_length):
+    pass
+
+def bleu(test_dataset, model, source_language, target_language, device):
+    pass
+
+
+#FROM ASSIGNMENT 4
+def compute_corpus_level_bleu_score(references: List[List[str]], hypotheses: List[Hypothesis]) -> float:
+    """ Given decoding results and reference sentences, compute corpus-level BLEU score.
+    @param references (List[List[str]]): a list of gold-standard reference target sentences
+    @param hypotheses (List[Hypothesis]): a list of hypotheses, one for each reference
+    @returns bleu_score: corpus-level BLEU score
+    """
+    # remove the start and end tokens
+    if references[0][0] == '<s>':
+        references = [ref[1:-1] for ref in references]
+    
+    # detokenize the subword pieces to get full sentences
+    detokened_refs = [''.join(pieces).replace('▁', ' ') for pieces in references]
+    detokened_hyps = [''.join(hyp.value).replace('▁', ' ') for hyp in hypotheses]
+
+    # sacreBLEU can take multiple references (golden example per sentence) but we only feed it one
+    bleu = sacrebleu.corpus_bleu(detokened_hyps, [detokened_refs])
+
+    return bleu.score
